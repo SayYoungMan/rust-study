f06dbf0c7ddd2b907e5c74549ef88e5c50fee0a0
SayYoungMan <nickte89@gmail.com>
2023-12-02 13:26:35 +0000

    [The Book] 12.2. Reading a File Done

--- The Book/12. An IO Project Building a Command Line Program/minigrep/src/main.rs
+++ The Book/12. An IO Project Building a Command Line Program/minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 fn main() {
     // First value of the vector is the name of our binary
     let args: Vec<String> = env::args().collect();
@@ -8,4 +8,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
